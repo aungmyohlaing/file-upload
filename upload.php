235067124a2feed320b4b6d7d2759ae545123606
Aung Myo Hlaing <amhlaing@gmail.com>
2016-07-19 13:58:23 +0700

    Update the Upload php and added upload image

--- upload.php
+++ upload.php
@@ -3,9 +3,9 @@
      $target_dir = "./upload/";
      
      print_r($_FILES);
-     $target_file = $target_dir . basename($_FILES["file"]["name"]);
+     $target_file = $target_dir . basename($_FILES["file_upload"]["name"]);
 
-     move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+     move_uploaded_file($_FILES["file_upload"]["tmp_name"], $target_file);
 
      
 ?>
